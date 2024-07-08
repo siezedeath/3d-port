be60fa15ba5924993aa0dddc6921683acf5158c5
Alfayed Alam <fayedfardil2@gmail.com>
2024-07-08 16:30:12 -0400

    starting the navbar, so far just uploaded a bunch of "template" tools (colors,. animations, etc)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
+
+// we will have logo, name, about, work, and contact
+// top of the page
+
+const Navbar = () => {
+  const [active, setActive] = useState('')
+
+
+  return (
+    <nav
+    className={"${styles.paddingX} w-full flex itmes-center py-5 fixed top=0 z-20 bg-primay"}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+        to="/"
+        className="flex items-center gap-2"
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        </Link>
+
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
